ca492e34c4cdac2c689d5ca36514b3fde8c320e6
Mark Vayngrib <mark@tradle.io>
2018-05-26 19:15:12 -0400

    export tx data field

--- index.js
+++ index.js
@@ -172,7 +172,8 @@ function createBlockchainAPI ({ network, networkName, apiKey }) {
       },
       to: {
         addresses: [txInfo.to].map(unprefixHex)
-      }
+      },
+      data: unprefixHex(txInfo.input || '')
     }
   }
 
